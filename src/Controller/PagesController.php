b1e383b9873aebf1d3fcdbe6f3b2ddcd8ad0058f
Amit Singh <amit.singh@kiwitech.com>
2018-01-02 12:31:16 +0530

    apidoc folder structure

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -12,6 +13,7 @@
  * @since     0.2.9
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
+
 namespace App\Controller;
 
 use Cake\Core\Configure;
@@ -26,8 +28,11 @@ use Cake\View\Exception\MissingTemplateException;
  *
  * @link https://book.cakephp.org/3.0/en/controllers/pages-controller.html
  */
-class PagesController extends AppController
-{
+class PagesController extends AppController {
+    
+    public function apiList(){
+       $this->render('api_list',false);
+    }
 
     /**
      * Displays a view
@@ -38,8 +43,7 @@ class PagesController extends AppController
      * @throws \Cake\Network\Exception\NotFoundException When the view file could not
      *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
      */
-    public function display(...$path)
-    {
+    public function display(...$path) {
         $count = count($path);
         if (!$count) {
             return $this->redirect('/');
@@ -66,4 +70,5 @@ class PagesController extends AppController
             throw new NotFoundException();
         }
     }
+
 }
