85ea035a55e2dd9531dd44deb86a0c957fe2aa55
Amit Singh <amit.singh@kiwitech.com>
2018-01-04 21:37:30 +0530

    Registration with matrix as well as spayc has been completed and data has been synced properly

--- config/spayc.php
+++ config/spayc.php
@@ -1,5 +1,10 @@
 <?php
 return [
     'title' =>'SPAYC',
-    'adminEmail' =>'kiwitech@gmail.com'
+    'adminEmail' =>'kiwitech@gmail.com',
+    'MATRIX'=>[
+        'url'=>'https://localhost:8448/_matrix/client/r0',
+        'sslHost' => false,
+        'sslPeer'=>false,        
+    ]
 ];
