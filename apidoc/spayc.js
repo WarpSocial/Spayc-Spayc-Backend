9ac214e2005fc28dc74c375d063805b68a192132
dhiru <dhirendra.singh@kiwitech.com>
2018-02-23 15:43:15 +0530

    apidoc updated

--- apidoc/spayc.js
+++ apidoc/spayc.js
@@ -142,7 +142,9 @@ function postSpaycs() { return; }
                 "subscribed_users": 0,
                 "friends": 0,
                 "joined_spayc_status": null,
+                "is_joined": false,
                 "joined_users": 0,
+                "is_subscribed": false,
                 "total_comments": 0,
                 "total_presents": 0
             },
@@ -161,7 +163,9 @@ function postSpaycs() { return; }
                 "subscribed_users": 1,
                 "friends": 0,
                 "joined_spayc_status": "Pending",
+                "is_joined": false,
                 "joined_users": 3,
+                "is_subscribed": true,
                 "total_comments": 1,
                 "total_presents": 0
             }
