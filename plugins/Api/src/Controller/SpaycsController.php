740e9578778b111944f5d85af6082ca9ffda4548
Amit Singh <amit.singh@kiwitech.com>
2018-02-28 12:09:57 +0530

    modification in date validation

--- plugins/Api/src/Controller/SpaycsController.php
+++ plugins/Api/src/Controller/SpaycsController.php
@@ -444,6 +444,53 @@ class SpaycsController extends AppController {
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
     public function edit($id = null) {
+        if (!$this->request->is(['put','patch','post'])) {
+            $this->restException(['status'=>'failed', 'message'=> __('Method not allowed.')], 405);
+        }
+        $data = $this->request->getData();
+        $data['group_type'] = !empty($data['group_type'])?ucfirst($data['group_type']):'';
+        if(empty($data['spayc_id'])) {
+            $this->restException(['status'=>'failed','message'=>'Spayc id is required.'], 400);
+        }
+        $entities = $this->Spaycs->find()->where(['id'=>$data['spayc_id']]);
+        if($entities->isEmpty()){
+            $this->restException(['status'=>'failed','message'=>__('Space has not been found.')], 400);
+        }
+        $entity = $entities->first();
+        $items = $this->Spaycs->patchEntity($entity, $data);        
+        if(!empty($items->errors())) {
+            $this->restException(['status'=>'failed','message'=>$this->mapErrors($items->errors())], 400);
+        }
+        $this->loadComponent('Api.Matrix');
+        $data['matrix_token'] = $this->Auth->user('UserLogs.matrix_access_token');
+        $matrix = $this->Matrix->updateRoom($entity->matrix_room_id,$data);
+        if(!empty($matrix['error'])) {
+            $this->restException(['status' => "failed", 'message' =>__($matrix['error'])], 400);
+        }
+        //$items->set('matrix_room_id',$matrix['room_id']);
+        //$items->set('matrix_room_alias',$matrix['room_alias']);
+        //$items->set('user_id', $this->Auth->user('id'));
+        
+        if($this->Spaycs->save($items)){
+             if(!empty($items['description'])) {
+                TableRegistry::get('Api.Hashtags')->saveHashTags($items['description'], $items['id']);
+            }
+            $this->response->statusCode(201);
+            $response = ['status'=>'success','message'=>__('Your spayc '.ucfirst($data['name']).', has been created.'),'data'=>$items];
+            /*Event to bind to update the set upload room image */
+            $event = new Event('Controller.Spayc.matrixMedia', $this->Controller, [
+                'options' => [
+                    'matrix_token'=>$data['matrix_token'],
+                    'image'=> $items->image,
+                    'matrix_room_id'=> $items->matrix_room_id,
+                    ]
+            ]);
+            EventManager::instance()->dispatch($event);
+        }else{
+            $this->restException(['status'=>'failed', 'message'=>__('The spayc could not be saved. Please, try again.')], 400);
+        }
+        $this->set($response);
+        die;
         $spayc = $this->Spaycs->get($id, [
             'contain' => []
         ]);
