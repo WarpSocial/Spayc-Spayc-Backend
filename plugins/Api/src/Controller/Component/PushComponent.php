67f0c46dce3f918caad015b8558f6a20d3c07262
dhiru <dhirendra.singh@kiwitech.com>
2018-03-20 16:00:44 +0530

    push notification changes

--- plugins/Api/src/Controller/Component/PushComponent.php
+++ plugins/Api/src/Controller/Component/PushComponent.php
@@ -77,7 +77,6 @@ class PushComponent extends Component {
             /*End ReCreate EndpointARN*/
             $par["action-loc-key"]="PLAY";
             $par["body"]=$message;
-             //APNS
             $FinalMessage = json_encode(array('default' => $message, 'APNS_SANDBOX' => json_encode(array(
                     'aps' => array(
                       'alert' => $message,
