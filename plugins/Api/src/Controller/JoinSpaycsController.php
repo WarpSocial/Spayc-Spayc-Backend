4eb3f34b1118673b4da22abfe36bea627d78645c
dhiru <dhirendra.singh@kiwitech.com>
2018-03-27 11:33:52 +0530

    join sub spyac

--- plugins/Api/src/Controller/JoinSpaycsController.php
+++ plugins/Api/src/Controller/JoinSpaycsController.php
@@ -151,7 +151,7 @@ class JoinSpaycsController extends AppController {
                 $jsModel->getConnection()->commit();
                 $friends = TableRegistry::get('Api.FriendRequest')->getFriendIdsByUserId($user['id'], 'Accepted');
                 //$userIds = $jsModel->getJoinedUserIds($data['spayc_id']);
-                if(in_array($spayc->user_id, $friends)) {
+                if($friends && in_array($spayc->user_id, $friends)) {
                     $push['slug'] = 'friend-join-spayc';
                 } else {
                     $push['slug'] = 'user-joined-your-spayc';
