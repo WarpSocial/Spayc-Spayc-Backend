8548f03ecf51093df32791cf65a00b09c6363c08
Sachin Chhabra <sachin.chhabra@kiwitech.com>
2018-04-16 19:33:28 +0530

    Bugs

--- webroot/apidoc/api_project.js
+++ webroot/apidoc/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-04-13T14:23:28.662Z",
+    "time": "2018-04-16T14:02:56.830Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
