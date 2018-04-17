899c1167deef182cab5751fe803fd6bc35d34116
Amit Singh <amit.singh@kiwitech.com>
2018-04-17 17:37:32 +0530

    push for subcri

--- plugins/Api/src/Controller/Component/PushComponent.php
+++ plugins/Api/src/Controller/Component/PushComponent.php
@@ -137,6 +137,10 @@ class PushComponent extends Component {
                 $notificationType->message = str_replace("<USERNAME>", ucwords($data['display_name']), $notificationType->message);
                 $notificationType->message = str_replace("<SpaycName>", $data['spayc_name'], $notificationType->message);
             }
+            if($notificationType->slug == 'user-subscribed-to-your-spayc' || $notificationType->slug == 'friend-subscribed-to-your-spayc') {
+                $notificationType->message = str_replace("<USERNAME>", ucwords($data['display_name']), $notificationType->message);
+                $notificationType->message = str_replace("<SpaycName>", $data['spayc_name'], $notificationType->message);
+            }
             
             $userImages = TableRegistry::get("Api.UserImages")->findByUserIdAndIsProfile($data['requested_by'], 'Yes');
             if(!$userImages->isEmpty()) {
