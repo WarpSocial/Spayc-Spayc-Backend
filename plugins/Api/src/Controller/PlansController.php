e5e2b2370a7344a40e8a9f5cd52414a6faba27d4
Sachin Chhabra <sachin.chhabra@kiwitech.com>
2018-05-18 15:35:15 +0530

    set time only event

--- plugins/Api/src/Controller/PlansController.php
+++ plugins/Api/src/Controller/PlansController.php
@@ -479,12 +479,13 @@ class PlansController extends AppController {
         }
         
         $data=$ad->first();
+        if($data->spayc['type']=='Event'){
         $timezone = Configure::read('timezone');
         $sd = new \Cake\I18n\Time($data->spayc['start_date'], 'UTC');
         $data->spayc['start_date'] = $sd->setTimezone(new \DateTimeZone($timezone))->format('Y-m-d H:i:s');
         $ed = new \Cake\I18n\Time($data->spayc['end_date'], 'UTC');
         $data->spayc['end_date'] = $ed->setTimezone(new \DateTimeZone($timezone))->format('Y-m-d H:i:s');
-        
+        }
         $data['frequency']=$frequency;
         $response = ['status' => 'success', 'message' => __('Promotion Find Successfully'), 'data' => $data];
         $this->set($response);
