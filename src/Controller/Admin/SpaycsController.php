2fd798eb3d8af827f563bfe985a92a153e482cb0
Sachin Chhabra <sachin.chhabra@kiwitech.com>
2018-05-30 20:24:47 +0530

    Push Block

--- src/Controller/Admin/SpaycsController.php
+++ src/Controller/Admin/SpaycsController.php
@@ -299,13 +299,17 @@ class SpaycsController extends AdminController
                 $spayc_id=$spayc->id;
 //                $spayc =$this->Spaycs->get($spayc->id);
                  $spaycs = $this->Spaycs->find();
-        $spaycs->select()            
-             ->where(['id'=>$spayc->id])
-                ->contain([
-                 'JoinedSpayc' => function($q) {
-                     return $q->select(['JoinedSpayc.id','JoinedSpayc.spayc_id','JoinedSpayc.user_id', 'JoinedSpayc.status','JoinedSpayc.distance'])->where(['JoinedSpayc.status'=>JOINED]);
-                 }   ]);
-            
+                 $spaycs->select()            
+                    ->where(['id'=>$spayc->id])
+                    ->contain([
+                     'JoinedSpayc' => function($q) {
+                         return $q->select(['JoinedSpayc.id','JoinedSpayc.spayc_id','JoinedSpayc.user_id', 'JoinedSpayc.status','JoinedSpayc.distance'])->where(['JoinedSpayc.status'=>JOINED]);
+                     },   
+                     'JoinedSpayc.users' => function($q) {
+                         return $q->select(['Users.email']);
+                     }   
+                     ]);
+                     $spaycs=$spaycs->first();
 //                  $spaycs->formatResults(function (\Cake\Collection\CollectionInterface $results) use($spayc_id){
 //                          return $results->map(function ($row) use($spayc_id) {
 //                $row['joined_users'] = TableRegistry::get('JoinedSpayc')->getJoinedUserIds($spayc_id);
@@ -317,7 +321,7 @@ class SpaycsController extends AdminController
 //                return $row;
 //            });
 //                  });
-                  // pr($spaycs->toArray());die;
+//                 pr($spaycs);die;
                 $displayName = !empty($spayc->name)? ucfirst($spayc->name) :SITE_TITLE;
                 if (ucfirst($spayc->status) == $statusArr['active']) {
                     $spayc->statusTxt = $txtMassage['unblock'];
@@ -330,14 +334,17 @@ class SpaycsController extends AdminController
                     $result_arr = ['result' => true, 'status'=>$statusArr['inactive'], 'message' => $displayName.' '.$this->errorSuccessMessage['BLOCKED-MSG']];   
                      $update=$this->inactiveSubSpaycStatus($spayc->id,$statusArr['inactive']);
                 }
-                if(!empty($spayc->email))
-                    $this->getMailer('User')->send('userStatus', [$spayc]);   
+                if(!empty($spaycs['joined_spayc']))
+                    foreach($spaycs['joined_spayc'] as $val){
+                    $spayc['email']=$val['Users']['email'];
+//                    $this->getMailer('User')->send('spaycStatus', $spayc);   
                 // for push notification
                 $push['requested_by'] = $this->Auth->user('id');
                 $push['username'] = $this->Auth->user('display_name');
-                $push['requested_to'] = $spayc->id;
+                $push['requested_to'] = $val->user_id;
                 $push['slug'] = $pushNotificationAdminSlug;
                 $this->Push->sendPushNotification($push);
+                    }
             } else {                
                 $result_arr = ['result' => false, 'status'=>'', 'message' => $this->errorSuccessMessage['SYSTEMERR']];   
             }
