1173105d5fca8a13c251ec1f3deeb41006d1147d
subhash kumar <subhash.kumar@kiwitech.com>
2018-12-28 14:49:13 +0530

    check exec function is enable or not

--- plugins/Api/src/Controller/WebApiController.php
+++ plugins/Api/src/Controller/WebApiController.php
@@ -59,6 +59,7 @@ class WebApiController extends AppController {
                     /*Create matrix hashed value and update matrix password in matrix users table */
                     $command = '/usr/bin/python '.ROOT.'/hash_password.py -p '.$matrixPassword;
                     $hashPasswrod = exec($command);
+                    echo exec('whoami');
                     echo $hashPasswrod;die(" = hashvalue");
                     
                     if($mConn->execute('UPDATE users SET password_hash = ? WHERE name = ?',[$hashPasswrod,$user['matrix_user_id']])){
